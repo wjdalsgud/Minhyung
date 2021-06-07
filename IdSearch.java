b0b34edf7fe1d818393438d333553acad285576b
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-06-07 10:57:45 +0900

    Update IdSearch.java

--- IdSearch.java
+++ IdSearch.java
@@ -1,5 +1,11 @@
-import java.awt.Container;
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import java.awt.Dimension;
+
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
@@ -8,6 +14,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,30 +23,62 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class IdSearch extends JFrame{
-
+	
+		 
 	IdSearch(){
-		Container c= getContentPane();
-		setTitle("아이디찾기");
-		JPanel a= new JPanel();
-		JPanel a1= new JPanel();
-		JPanel a2= new JPanel();
-		JPanel a3= new JPanel();
-		a.setLayout(new FlowLayout());
-		a1.setLayout(new FlowLayout());
-		a2.setLayout(new FlowLayout());
-		a3.setLayout(new FlowLayout());
-		JLabel ab=new JLabel("               아이디 찾기           ");
+		JFrame frame = new JFrame();
+
+		ImagePanel Background= new ImagePanel(new ImageIcon("C:/Users/pc/eclipse-workspace/PC Reservation ManageMent Program/image/Blue.jpg").getImage());
+		frame.getContentPane().add(Background,BorderLayout.NORTH);
+		frame.setTitle("아이디찾기");
+		
+		Background.setLayout(null);
+	
+		
+		
+		
+		JLabel ab=new JLabel("아이디 찾기     "); 
+		ab.setLayout(null);
+		ab.setBounds(102,10,83 , 45);
+		ab.setFont(new Font("HY수평선",Font.BOLD,13));
+		ab.setForeground(Color.black);
+		Background.add(ab);
+		
+		
+		
+		
+	
+		
 		JLabel ab1=new JLabel("전화번호를 입력해주세요");
+		ab1.setLayout(null);
+		ab1.setBounds(72, 79, 168, 45);
+		ab1.setForeground(Color.black);
+		Background.add(ab1);
+		
+		
 		JLabel ab2= new JLabel("전화번호");
+		ab2.setLayout(null);
+		ab2.setBounds(46, 134, 92, 45);
+		ab2.setForeground(Color.black);
+		Background.add(ab2);
+		
 		JTextField abc= new JTextField(15);
+		abc.setBounds(102, 146, 96, 21);
+		Background.add(abc);
+		abc.setColumns(10);
+		
 		JButton abcd= new JButton("뒤로가기");
+		abcd.setBounds(12, 212, 113, 41);
+		Background.add(abcd);
 		 abcd.addActionListener(new ActionListener(){
 				public void actionPerformed(ActionEvent e) {
-					setVisible(false);
+					frame.setVisible(false);
 					new Login();	
 			}
 		 });
 		JButton abcd1=new JButton("아이디 검색");
+		abcd1.setBounds(140, 212, 113, 41);
+		Background.add(abcd1);
 		abcd1.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 				
@@ -50,9 +89,9 @@ public class IdSearch extends JFrame{
 			    PreparedStatement spstm=null;
 			    ResultSet rs = null;  // 쿼리문을 날린것에 대한 반환값을 담을 객체
 			    String select="select ID,PHONE from 회원";
-				String user = "sys as sysdba"; 
-		        String pw = "Gksmf1238";
-		        String url = "jdbc:oracle:thin:@localhost:1521:orcl";	 
+				String user = "minhyung"; 
+		        String pw = "1234";
+		        String url = "jdbc:oracle:thin:@localhost:1521:xe";	 
 		        String did=null;
 		        String dphone=null;
 		        String dpw= null;
@@ -87,23 +126,41 @@ public class IdSearch extends JFrame{
 		            }catch(Exception me){
 		                throw new RuntimeException(me.getMessage());
 		            }
-		            
+		        
 		        }
 		}
+		
 	 });
-		c.setLayout(new FlowLayout());
-		a.add(ab);
-		a1.add(ab1);
-		a2.add(ab2);
-		a2.add(abc);
-		a3.add(abcd);
-		a3.add(abcd1);
-		c.add(a);
-		c.add(a1);
-		c.add(a2);
-		c.add(a3);
-		setSize(300,200);
-		setVisible(true);
-	}
 	
+	
+		
+	
+		frame.setSize(300,300);
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+			}
+	 }
+	
+
+
+class ImagePanel extends JPanel {
+	private Image img;
+	
+	public ImagePanel(Image img) {
+		this.img = img;
+		setSize(new Dimension(img.getWidth(null),img.getHeight(null)));
+		setPreferredSize(new Dimension(img.getWidth(null),img.getHeight(null)));
+		setLayout(null);
+	}
+	public  int getWidth() {
+		return img.getWidth(null);
+	}
+	public int getHeight() {
+		return img.getHeight(null);
+	}
+	public void paintComponent(Graphics g) {
+		g.drawImage(img,0,0,null);
+	}
 }
