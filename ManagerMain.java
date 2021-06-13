9650ad15b4ad80db5cb9a7e8895515a10206a5ca
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-06-13 20:51:29 +0900

    Update ManagerMain.java

--- ManagerMain.java
+++ ManagerMain.java
@@ -1,6 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Cursor;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -34,6 +35,9 @@ public class ManagerMain extends JFrame{
 		Background.add(MainLabel);
 		
 		JButton Confirm_btn = new JButton("회원 확인");
+		Confirm_btn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		Confirm_btn.setBackground(Color.LIGHT_GRAY);
+		Confirm_btn.setForeground(Color.BLUE);
 		 Confirm_btn.setLayout(null);
 		 Confirm_btn.setBounds(12, 72, 107, 42);
 		 Background.add(Confirm_btn);
@@ -48,6 +52,9 @@ public class ManagerMain extends JFrame{
 			 
 		 });
 		JButton Delete_member_btn = new JButton("회원 삭제");
+		Delete_member_btn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		Delete_member_btn.setBackground(Color.LIGHT_GRAY);
+		Delete_member_btn.setForeground(Color.BLUE);
 		Delete_member_btn.setLayout(null);
 		Delete_member_btn.setBounds(12, 138, 107, 42);
 		Background.add(Delete_member_btn);
@@ -68,6 +75,9 @@ public class ManagerMain extends JFrame{
 		 
 		 
 		JButton Revise_member_btn = new JButton("회원 수정");
+		Revise_member_btn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		Revise_member_btn.setBackground(Color.LIGHT_GRAY);
+		Revise_member_btn.setForeground(Color.BLUE);
 		Revise_member_btn.setLayout(null);
 		Revise_member_btn.setBounds(12, 209, 107, 42);
 		Background.add(Revise_member_btn);
@@ -83,6 +93,9 @@ public class ManagerMain extends JFrame{
 		});
 		
 		JButton Manage_seat_btn = new JButton("좌석 관리");
+		Manage_seat_btn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		Manage_seat_btn.setBackground(Color.LIGHT_GRAY);
+		Manage_seat_btn.setForeground(Color.BLUE);
 		Manage_seat_btn.setLayout(null);
 		Manage_seat_btn.setBounds(99, 283, 107, 42);
 		Background.add( Manage_seat_btn);
@@ -97,6 +110,9 @@ public class ManagerMain extends JFrame{
 		});
 		 
 		 JButton Logout_btn = new JButton("로그아웃");
+		 Logout_btn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		 Logout_btn.setBackground(Color.LIGHT_GRAY);
+		 Logout_btn.setForeground(Color.BLUE);
 		 Logout_btn .setLayout(null);
 		 Logout_btn.setBounds(193, 72, 107, 42);
 		 Background.add(Logout_btn);
@@ -113,6 +129,9 @@ public class ManagerMain extends JFrame{
 		 
 		 
 		 JButton Exit_btn = new JButton("종료");
+		 Exit_btn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		 Exit_btn.setBackground(Color.LIGHT_GRAY);
+		 Exit_btn.setForeground(Color.BLUE);
 		 Exit_btn.setLayout(null);
 		 Exit_btn.setBounds(193, 138, 107, 42);
 		 Background.add(Exit_btn);
@@ -127,6 +146,9 @@ public class ManagerMain extends JFrame{
 			 
 		 });
 		 JButton go_user = new JButton("사용자 화면으로");
+		 go_user.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		 go_user.setBackground(Color.LIGHT_GRAY);
+		 go_user.setForeground(Color.BLUE);
 		 go_user.setLayout(null);
 		 go_user.setBounds(181, 209, 145, 42);
 		 Background.add(go_user);
