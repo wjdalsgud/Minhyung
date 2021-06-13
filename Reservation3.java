43a0d4d1d113ead238a74086dc30b7435e6a1c89
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-06-13 20:53:45 +0900

    Update Reservation3.java

--- Reservation3.java
+++ Reservation3.java
@@ -1,6 +1,7 @@
-iimport java.awt.BorderLayout;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Cursor;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -30,11 +31,14 @@ public class Reservation3 extends JFrame{
 			  j1=new JLabel("현재 예약된 좌석은 "+LoginNextMain.dseat+"번입니다.");
 		    }
 		j1.setFont(new Font("HT수평선",Font.BOLD,13));
-		j1.setForeground(Color.WHITE);
+		j1.setForeground(Color.BLACK);
 		j1.setBounds(102, 138, 214, 43);
 		Background.add(j1);
 		
 		JButton btnNewButton = new JButton("뒤로 가기");
+		btnNewButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		btnNewButton.setBackground(Color.LIGHT_GRAY);
+		btnNewButton.setForeground(Color.BLUE);
 		btnNewButton.setBounds(284, 131, 111, 57);
 		Background.add(btnNewButton);
 		btnNewButton.addActionListener(new ActionListener() {
