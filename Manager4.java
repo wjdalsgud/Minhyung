4ec04f26861624dadf5989c0ca1d219239c9e323
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-06-14 22:20:58 +0900

    Update Manager4.java

--- Manager4.java
+++ Manager4.java
@@ -1,52 +1,67 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.Cursor;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Manage4 extends JFrame {
-	
+public class Manager4 extends JFrame {
 
-	
-		JCheckBox[] place=new JCheckBox[14];
-		String[] name = {"1번자리","2번자리","3번자리","4번자리","5번자리","6번자리","7번자리","8번자리","9번자리","10번자리","11번자리","12번자리","13번자리","14번자리"};
-		Manage4(){
-				 setTitle("좌석 관리 화면");
-				 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				 Container c=getContentPane();
-				 c.setLayout(new FlowLayout(FlowLayout.LEFT,5,40));
-				 for(int i=0;i<place.length;i++) {
-					 place[i]=new JCheckBox(name[i]);
-					 place[i].setBorderPainted(true);
-					 c.add(place[i]);
-				 }
-				 JButton b1=new JButton("뒤로가기");
-				 b1.addActionListener(new ActionListener(){
-						public void actionPerformed(ActionEvent e) {
-							setVisible(false);
-							new ManagerMain();	
-					}
-				 });
-				 JButton b2=new JButton("다음화면");
-				 b2.addActionListener(new ActionListener(){
-						public void actionPerformed(ActionEvent e) {
-							setVisible(false);
-							new  ManerSet();	
-					}
-				 });
-				 JPanel a= new JPanel();
-				 a.setLayout(new FlowLayout(FlowLayout.CENTER,140,40));
-				 a.add(b1);
-				 a.add(b2);
-				 c.add(a);
-
-				setVisible(true);
-				setSize(400,400);
+	Manager4(){
+		JFrame frame = new JFrame();
+		ImagePanel Background= new ImagePanel(new ImageIcon("C:/Users/pc/eclipse-workspace/PC Reservation ManageMent Program/image/City.jpg").getImage());
+		frame.getContentPane().add(Background,BorderLayout.NORTH);
+		frame.setTitle("좌석 관리 화면");
 		
-	}
+		
+		
+		Background.setLayout(null);
+		
+		JButton back = new JButton ("뒤로 가기");
+		back.setLayout(null);
+		back.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		back.setBackground(Color.LIGHT_GRAY);
+		back.setForeground(Color.BLUE);
+		back.setBounds(12, 115, 105, 35);
+		Background.add(back);
+		back.addActionListener(new ActionListener() {
 
-}
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				frame.setVisible(false);
+				new ManagerMain();
+				
+			}
+			
+		});
+		JButton b2 = new JButton("좌석 수 변경");
+		b2.setLayout(null);
+		b2.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		b2.setBackground(Color.LIGHT_GRAY);
+		b2.setForeground(Color.BLUE);
+		b2.setBounds(149, 115, 105, 35);
+		Background.add(b2);
+		b2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				frame.setVisible(false);
+				new ChangeSeat();
+			}
+			
+		});
+		frame.setSize(300,300);
+		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		}
+	}
