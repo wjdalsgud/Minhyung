db5fbb2c5eddee84535bb2d8a9eefa4691b4dfe7
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-06-15 02:41:25 +0900

    Update IdSearch.java

--- IdSearch.java
+++ IdSearch.java
@@ -1,158 +1,69 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Cursor;
-import java.awt.Dimension;
-
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.Scanner;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+public class InsertSeat {
 
-public class IdSearch extends JFrame{
-	
-		 
-	IdSearch(){
-		JFrame frame = new JFrame();
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Connection conn = null; // DB연결된 상태(세션)을 담은 객체
+	    PreparedStatement pstm = null;  // SQL 문을 나타내는 객체
+	    ResultSet rs = null;  // 쿼리문을 날린것에 대한 반환값을 담을 객체
+	    String sql="insert into 피시방좌석 values(?,?,?)";
+	    String select="select ID from 회원";
+		String user = "minhyung"; 
+        String pw1 = "1234";
+        String url = "jdbc:oracle:thin:@localhost:1521:xe";	                
 
-		ImagePanel Background= new ImagePanel(new ImageIcon("./image/paint2.jpg").getImage());
-		frame.getContentPane().add(Background,BorderLayout.NORTH);
-		frame.setTitle("아이디찾기");
-		
-		Background.setLayout(null);
-	
-		
-		JLabel ab=new JLabel("아이디 찾기"); 
-		ab.setLayout(null);
-		ab.setBounds(102,10,83 , 45);
-		ab.setFont(new Font("HY수평선",Font.BOLD,13));
-		ab.setForeground(Color.black);
-		Background.add(ab);
-		
-		JLabel ab1=new JLabel("전화번호를 입력해주세요");
-		ab1.setLayout(null);
-		ab1.setBounds(72, 79, 168, 45);
-		ab1.setForeground(Color.black);
-		Background.add(ab1);
-		
-		
-		JLabel ab2= new JLabel("전화번호");
-		ab2.setLayout(null);
-		ab2.setBounds(46, 134, 92, 45);
-		ab2.setForeground(Color.black);
-		Background.add(ab2);
-		
-		JTextField abc= new JTextField(15);
-		abc.setBounds(102, 146, 96, 21);
-		Background.add(abc);
-		abc.setColumns(10);
-		
-		JButton abcd= new JButton("뒤로가기");
-		abcd.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		abcd.setBackground(Color.LIGHT_GRAY);
-		abcd.setForeground(Color.BLUE);
-		abcd.setBounds(12, 212, 113, 41);
-		Background.add(abcd);
-		 abcd.addActionListener(new ActionListener(){
-				public void actionPerformed(ActionEvent e) {
-					frame.setVisible(false);
-					new Login();	
-			}
-		 });
-		JButton abcd1=new JButton("아이디 검색");
-		abcd1.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		abcd1.setBackground(Color.LIGHT_GRAY);
-		abcd1.setForeground(Color.BLUE);
-		abcd1.setBounds(140, 212, 113, 41);
-		Background.add(abcd1);
-		abcd1.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				
-				String id,phone;
-				phone=abc.getText();
-				Connection conn = null; // DB연결된 상태(세션)을 담은 객체
-			    PreparedStatement pstm = null;  // SQL 문을 나타내는 객체
-			    PreparedStatement spstm=null;
-			    ResultSet rs = null;  // 쿼리문을 날린것에 대한 반환값을 담을 객체
-			    String select="select ID,PHONE from 회원";
-				String user = "minhyung"; 
-		        String pw = "1234";
-		        String url = "jdbc:oracle:thin:@localhost:1521:xe";	 
-		        String did=null;
-		        String dphone=null;
-		        String dpw= null;
-		        try {
-		        Class.forName("oracle.jdbc.driver.OracleDriver");      
-		        conn = DriverManager.getConnection(url, user, pw);	       
-		    	spstm=conn.prepareStatement(select);
-		    	rs=spstm.executeQuery();
-		    	while(rs.next()) {
-		    		did=rs.getString("ID");
-		    		dphone= rs.getString("PHONE");
-		    		if(dphone.equals(phone)) {
-		    			JOptionPane.showMessageDialog(null, "ID : "+did,"Message",JOptionPane.INFORMATION_MESSAGE);
-		    			break;
-		    		}
-		    	}
-		    	if(!dphone.equals(phone)) {
-		    		JOptionPane.showMessageDialog(null, "전화번호를 잘못 입력했습니다","Message",JOptionPane.ERROR_MESSAGE);
-				}
-		         } catch (SQLException be) {
-		        	 System.out.println("sql오류");
-		        	 be.printStackTrace();
-		        }catch(Exception ne) {
-		        	System.out.println("오류");
-		        }
-		        finally{
-		            // DB 연결을 종료한다.
-		            try{
-		                if ( rs != null ){rs.close();}   
-		                if ( pstm != null ){pstm.close();}   
-		                if ( conn != null ){conn.close(); }
-		            }catch(Exception me){
-		                throw new RuntimeException(me.getMessage());
-		            }
-		        
-		        }
-		}
-		
-	 });
-		frame.setSize(300,300);
-		frame.setVisible(true);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
-			}
-	 }
-class ImagePanel extends JPanel {
-	private Image img;
-	
-	public ImagePanel(Image img) {
-		this.img = img;
-		setSize(new Dimension(img.getWidth(null),img.getHeight(null)));
-		setPreferredSize(new Dimension(img.getWidth(null),img.getHeight(null)));
-		setLayout(null);
-	}
-	public  int getWidth() {
-		return img.getWidth(null);
-	}
-	public int getHeight() {
-		return img.getHeight(null);
-	}
-	public void paintComponent(Graphics g) {
-		g.drawImage(img,0,0,null);
-	}
+        try {
+        	  Class.forName("oracle.jdbc.driver.OracleDriver");  
+        	  conn = DriverManager.getConnection(url, user, pw1);	    
+            // SQL 문장을 만들고 만약 문장이 질의어(SELECT문)라면
+            // 그 결과를 담을 ResulSet 객체를 준비한 후 실행시킨다.
+            String quary = "SELECT ID,PW,PHONE FROM 관리자";
+            System.out.println("============================================");
+            Scanner sc= new Scanner(System.in);
+            String a=sc.nextLine();
+            if(a.equals("select")) {
+
+                pstm = conn.prepareStatement(quary);
+                rs = pstm.executeQuery();
+            while(rs.next()){
+                String id = rs.getString("ID");
+                String pw = rs.getString("PW");
+                String phone = rs.getString("PHONE");
+                String result = id+" "+pw+" "+ phone;
+                System.out.println(result);
+            }
+            }
+            else if(a.equals("insert")){
+            	String SQL="insert into 피시방좌석 values(?,?,?)";
+                pstm = conn.prepareStatement(SQL);
+                for(int i=1;i<1001;i++) {
+            	pstm.setInt(1, 1); 
+            	pstm.setInt(2, i); 
+            	pstm.setInt(3, 0);
+            	int r= pstm.executeUpdate();
+                }
+            }
+
+        } catch (Exception sqle) {
+            System.out.println("SELECT문에서 예외 발생");
+            sqle.printStackTrace();
+
+        }finally{
+            // DB 연결을 종료한다.
+            try{
+                if ( rs != null ){rs.close();}   
+                if ( pstm != null ){pstm.close();}   
+                if ( conn != null ){conn.close(); }
+            }catch(Exception e){
+                throw new RuntimeException(e.getMessage());
+            }
+
+        }
+
 }
+	}
