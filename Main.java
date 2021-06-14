9699e32293517055ac37d62b35863645a2fbe161
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-06-15 02:43:45 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Main {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Date nowDate = new Date(); 
